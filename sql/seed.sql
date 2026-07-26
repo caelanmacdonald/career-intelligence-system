131d0156bd2470299e3e5315c0ef03dcfca5ebcf
Caelan Macdonald <C43L4N@gmail.com>
2026-07-26 11:04:38 +0100

    Add jobs schema, seed data and test queries

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,37 +1,3 @@
-INSERT INTO companies (
-    company_name,
-    sector,
-    sponsor_status,
-    careers_url,
-    priority,
-    headquarters
-)
-VALUES
-    (
-        'Sky',
-        'Media',
-        'Licensed',
-        'https://careers.sky.com',
-        'A',
-        'London'
-    ),
-    (
-        'Tesco',
-        'Retail',
-        'Licensed',
-        'https://www.tesco-careers.com',
-        'A',
-        'Welwyn Garden City'
-    ),
-    (
-        'BBC',
-        'Media',
-        'Licensed',
-        'https://careers.bbc.co.uk',
-        'A',
-        'London'
-    );
-
 INSERT INTO jobs (
     company_id,
     job_title,
